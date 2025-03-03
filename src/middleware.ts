5bf98d163e6ecde4fad2a03f6484de92de4f1ec6
Muhammad Rana <ranatb2023@gmail.com>
2025-03-03 12:31:22 +0500

    Uploaded project on GIT

---
+++ src/middleware.ts
@@ -0,0 +1,40 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isPublicRoute = createRouteMatcher(['/site', '/api/uploadthing']);
+export default clerkMiddleware( async(auth, request) => {
+  
+    const url = request.nextUrl
+    const searchParams = url.searchParams.toString()
+    let hostname = request.headers
+
+    const pathWithSearchParams = `${url.pathname}${searchParams.length > 0 ? `?${searchParams}` : ''}`
+
+    //if subdomain exists
+    const customSubDomain = hostname.get('host')?.split(`${process.env.NEXT_PUBLIC_DOMAIN}`).filter(Boolean)[0]
+
+    if (customSubDomain) {
+      return NextResponse.rewrite(new URL(`/${customSubDomain}${pathWithSearchParams}`, request.url))
+    }
+
+    if (url.pathname === '/sign-in' || url.pathname === '/sign-up') {
+      return NextResponse.redirect(new URL(`/agency/sign-in`, request.url))
+    }
+
+    if (url.pathname === '/' || (url.pathname === '/site' && url.host === process.env.NEXT_PUBLIC_DOMAIN)){
+      return NextResponse.rewrite(new URL('/site', request.url))
+    }
+
+    if (url.pathname.startsWith('/agency') || url.pathname.startsWith('/subaccount')){
+      return NextResponse.rewrite(new URL(`${pathWithSearchParams}`, request.url))
+    }
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
